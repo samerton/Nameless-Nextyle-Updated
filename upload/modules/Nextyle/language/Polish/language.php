42abcc36255453d72b5274ab0cffe5997628cec1
samerton <samerton@users.noreply.github.com>
2019-01-09 19:07:15 +0000

    Add Polish translation
    Add Polish Nextyle module translation, thanks to brzezinsky

---
+++ upload/modules/Nextyle/language/Polish/language.php
@@ -0,0 +1,28 @@
+<?php 
+/*
+ *	Made by Nexious
+ *  https://nexious.net
+ *  NamelessMC version 2.0.0-pr5
+ *  License: MIT
+ *
+ *  Translated by brzezinsky
+ *
+ *  Polish Language - Nextyle for NamelessMC
+ *  Język polski - Nextyle dla NamelessMC
+ */
+
+// Which version of NamelessMC is this language file updated to?
+// Jaka wersja NamelessMC jest przeznaczona do tego pliku językowego?
+$language_version = '2.0.0-pr5';
+ 
+$language = array(
+	'nextyle_title' => 'Nextyle',
+	'theme' => 'Kolor szablonu',
+	'red' => 'Czerwony',
+	'blue' => 'Niebieski',
+	'gold' => 'Złoty',
+	'header_background' => 'Tło nagłówka',
+	'logo' => 'Logo szablonu',
+	'unable_to_write_to_settings' => 'Nie można nadpisać pliku <strong>modules/Nextyle/pages/settings.php</strong>. Sprawdź uprawnienia pliku.',
+	'successfully_updated' => 'Pomyślnie zaktualizowano.',
+);
